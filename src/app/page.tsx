b3891fd2f5ddf2c60619fa937a1f5801f0281f2f
AlekseyPudlo <pudloaleksey@gmail.com>
2024-02-19 21:22:50 +0100

    Add WeatherData type; Add small fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
+'use client';
+
 import Navbar from "@/components/Navbar";
+import { WeatherData } from "@/types/weather-data";
+import axios from "axios";
+import { useQuery } from "react-query";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>('repoData', async (): Promise<WeatherData> => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=Hamburg&appid=${process.env.PUBLIC_OPENWEATHERMAP_API_KEY}`);
+    return data;
+  });
+
+  console.log(data);
+  console.log(process.env.PUBLIC_OPENWEATHERMAP_API_KEY);
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
-      <h1>Hello World</h1>
-      
-      
     </div>
   );
+}
+function asyc(): Omit<import("react-query").UseQueryOptions<unknown, unknown, unknown, "repoData">, "queryKey"> | undefined {
+  throw new Error("Function not implemented.");
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,6 @@
-/** @format */
-
 import React from "react";
 import { MdMyLocation, MdOutlineLocationOn, MdWbSunny } from "react-icons/md";
-import SearchBox from "./Searchbox";
+import SearchBox from "./SearchBox";
 
 type Props = {}
 
@@ -12,12 +10,12 @@ export default function Navbar({}: Props) {
             <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-5 mx-auto">
                 <p className="flex items-center justify-center gap-2">
                     <h2 className="text-gray-500 text-3x1">Weather</h2>
-                    <MdWbSunny className="text-3x1 mt-1 text-yellow-400"/>
+                    <MdWbSunny className="text-3x1 mt-1 text-yellow-400" />
                 </p>
                 {/* Your navbar content goes here */}
                 <section className="flex gap-2 items-center">
-                    <MdMyLocation className="text-2x1 text-gray-600 hover:opacity-50 cursor-pointer"/>
-                    <MdOutlineLocationOn className="text-3x1"/>
+                    <MdMyLocation className="text-2x1 text-gray-600 hover:opacity-50 cursor-pointer" />
+                    <MdOutlineLocationOn className="text-3x1" />
                     <p className="text-slate-900/80 text-sm">Hamburg</p>
                     <div>
                         <SearchBox />
